c59202bc2953cd32b410baf13a520390a6de5dd7
Robert Baxter <baxterrp@gmail.com>
2022-05-15 11:27:00 -0400

    Add project files.

---
+++ RyansOrderApp/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RyansOrderApp.Models;
+using System.Diagnostics;
+
+namespace RyansOrderApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ryanContext ryanContext;
+
+        public HomeController(ryanContext ryanContext)
+        {
+            this.ryanContext = ryanContext;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult SubmitOrder(Order order)
+        {
+            // save to database
+            ryanContext.Orders.Add(order);
+            ryanContext.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Orders()
+        {
+            var orders = ryanContext.Orders.ToList();
+
+            return View(orders);
+        }
+    }
+}
---
+++ RyansOrderApp/Models/Order.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RyansOrderApp.Models
+{
+    public partial class Order
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Item { get; set; }
+    }
+}
